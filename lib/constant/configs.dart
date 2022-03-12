8c269de7fd1a63c5ce38208ad8f59c5d86a1cde1
Ko Htutz <info@kohtut.me>
2022-03-12 17:29:03 +0630

    fix : db nameg

--- lib/constant/configs.dart
+++ lib/constant/configs.dart
@@ -2,6 +2,7 @@ class Config {
   static const httpRequestTimeout = 10;
   // Foodball Api
   static const String prodfbUrl = "";
-  static const String uatfbUrl = "http://54.251.136.173";
+  // static const String uatfbUrl = "http://54.251.136.173";
+  static const String uatfbUrl = "https://football.app.kohtut.dev";
   static const String basefootballUrl = uatfbUrl;
 }
